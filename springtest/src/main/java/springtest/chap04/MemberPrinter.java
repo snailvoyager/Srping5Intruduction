22a3364f94c6764c7aa67d3d1e3d3d8c38c0ff82
snailvoyager <kyj88514@gmail.com>
2020-03-08 18:20:20 +0900

    자동 주입 필수여부 설정

--- springtest/src/main/java/springtest/chap04/MemberPrinter.java
+++ springtest/src/main/java/springtest/chap04/MemberPrinter.java
@@ -1,8 +1,25 @@
 package springtest.chap04;
 
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class MemberPrinter {
+	private DateTimeFormatter dateTimeFormatter;
+	
 	public void print(Member member) {
-		System.out.printf("ID = %d, email=%s, name=%s, regdate=%tF\n",
-				member.getId(), member.getEmail(), member.getName(), member.getRegisterDateTime());
+		if(dateTimeFormatter == null) {
+			System.out.printf("ID = %d, email=%s, name=%s, regdate=%tF\n",
+					member.getId(), member.getEmail(), member.getName(), member.getRegisterDateTime());
+		} else {
+			System.out.printf("ID = %d, email=%s, name=%s, regdate=%s\n",
+					member.getId(), member.getEmail(), member.getName(),
+					dateTimeFormatter.format(member.getRegisterDateTime()));	//날짜 형식을 맞춰 출력
+		}
+	}
+	
+	@Autowired(required=false)		//자동 주입할 대상이 필수가 아닌 경우. 자동주입할 Bean객체가 없으면 메서드를 실행하지 않는다
+	public void setDateFormatter(DateTimeFormatter dateTimeFormatter) {
+		this.dateTimeFormatter = dateTimeFormatter;
 	}
 }
