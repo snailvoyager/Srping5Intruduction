9df15338c8dc67b8162f72a9d8bd2b81b6885e36
snailvoyager <kyj88514@gmail.com>
2020-03-09 21:56:23 +0900

    생성자 초기화와 필수 여부 지정 방식

--- springtest/src/main/java/springtest/chap04/MemberPrinter.java
+++ springtest/src/main/java/springtest/chap04/MemberPrinter.java
@@ -1,11 +1,17 @@
 package springtest.chap04;
 
 import java.time.format.DateTimeFormatter;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
 
 public class MemberPrinter {
 	private DateTimeFormatter dateTimeFormatter;
+	
+	public MemberPrinter() {		//생성자 초기화
+		this.dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일");	//초기화를 하더라도 자동 주입 시 바뀔 수 있음
+	}
 	
 	public void print(Member member) {
 		if(dateTimeFormatter == null) {
@@ -18,8 +24,22 @@ public class MemberPrinter {
 		}
 	}
 	
-	@Autowired(required=false)		//자동 주입할 대상이 필수가 아닌 경우. 자동주입할 Bean객체가 없으면 메서드를 실행하지 않는다
-	public void setDateFormatter(DateTimeFormatter dateTimeFormatter) {
-		this.dateTimeFormatter = dateTimeFormatter;
+//	@Autowired(required=false)		//자동 주입할 대상이 필수가 아닌 경우. 자동주입할 Bean객체가 없으면 메서드를 실행하지 않는다
+//	public void setDateFormatter(DateTimeFormatter dateTimeFormatter) {
+//		this.dateTimeFormatter = dateTimeFormatter;
+//	}
+	
+//	@Autowired
+//	public void setDateFormatter(@Nullable DateTimeFormatter dateTimeFormatter) {	//의존 대상이 없으면 인자로 null값을 전달. 메서드는 호출
+//		this.dateTimeFormatter = dateTimeFormatter;
+//	}
+	
+	@Autowired
+	public void setDateFormatter(Optional<DateTimeFormatter> formatterOpt) {
+		if(formatterOpt.isPresent()) {
+			this.dateTimeFormatter = formatterOpt.get();	//의존 대상이 있으면 Optional 인자
+		} else {
+			this.dateTimeFormatter = null;
+		}
 	}
 }
