584b34b291783ce2fda4d3f1cfc48cc2305a4057
snailvoyager <kyj88514@gmail.com>
2020-03-09 23:35:50 +0900

    스캔 대상 제외하기

--- springtest/src/main/java/springtest/chap05/AppCtx.java
+++ springtest/src/main/java/springtest/chap05/AppCtx.java
@@ -3,11 +3,19 @@ package springtest.chap05;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 
 @Configuration
-@ComponentScan(basePackages = {"springtest.chap05"})	//스캔 대상 패키지 목록
+@ComponentScan(basePackages = {"springtest.chap05"},	//스캔 대상 패키지 목록
+	excludeFilters = @Filter(type = FilterType.ASSIGNABLE_TYPE, classes = MemberDao.class))	//스캔대상 제외
 public class AppCtx {
+	
+	@Bean
+	public MemberDao memberDao() {
+		return new MemberDao();
+	}
 	
 	@Bean
 	@Qualifier("printer")		//자동 주입 대상 Bean객체를 한정
