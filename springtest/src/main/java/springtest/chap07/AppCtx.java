285845662bbd86f7d1dafeb41fd442a0edec92a1
snailvoyager <kyj88514@gmail.com>
2020-03-12 22:54:28 +0900

    AOP Proxy 생성 방식

--- springtest/src/main/java/springtest/chap07/AppCtx.java
+++ springtest/src/main/java/springtest/chap07/AppCtx.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
-@EnableAspectJAutoProxy		//@Aspect 붙은 Bean객체를 찾아서 Pointcut, Around 설정을 사용
+@EnableAspectJAutoProxy(proxyTargetClass = true)		//@Aspect 붙은 Bean객체를 찾아서 Pointcut, Around 설정을 사용
 public class AppCtx {
 	@Bean
 	public ExeTimeAspect exeTimeAspect() {
@@ -13,7 +13,7 @@ public class AppCtx {
 	}
 	
 	@Bean
-	public Calculator calculator() {
+	public Calculator calculator() {	//AOP적용 시 RecCalculator가 상속받은 Interface를 이용해서 프록시를 생성
 		return new RecCalculator();
 	}
 }
--- springtest/src/main/java/springtest/chap07/MainAspect.java
+++ springtest/src/main/java/springtest/chap07/MainAspect.java
@@ -6,7 +6,11 @@ public class MainAspect {
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtx.class);
 		
-		Calculator cal = ctx.getBean("calculator", Calculator.class);		//프록시 객체를 리턴
+		//Calculator cal = ctx.getBean("calculator", Calculator.class);
+		RecCalculator cal = ctx.getBean("calculator", RecCalculator.class);		//프록시 객체를 리턴
+		//생성할 Bean객체가 인터페이스를 상속하면 인터페이스를 이용하여 프록시 생성
+		//Calculator 를 상속한 프록시 타입이면 RecCalculator 로 타입 변환 불가
+		//@EnableAspectJAutoProxy(proxyTargetClass=true) 속성 지정으로 자바 클래스를 상속받아 프록시 생성
 		long fiveFact = cal.factorial(5);
 		System.out.println("cal.factorial(5) = " + fiveFact);
 		System.out.println(cal.getClass().getName());
