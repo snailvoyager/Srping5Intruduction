6c240cd4dc67bc08e1e44affdc928e1f4eb933cb
snailvoyager <kyj88514@gmail.com>
2020-03-27 23:54:59 +0900

    컨트롤러 없는 경로 매핑

--- springtest/src/main/java/config/MvcConfig.java
+++ springtest/src/main/java/config/MvcConfig.java
@@ -3,6 +3,7 @@ package config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,5 +16,11 @@ public class MvcConfig implements WebMvcConfigurer{
 	
 	public void configureViewResolvers(ViewResolverRegistry registry) {
 		registry.jsp("/WEB-INF/view/", ".jsp");
+	}
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {	//컨트롤러 구현없이 요청경로와 뷰 이름을 연결
+		// TODO Auto-generated method stub
+		registry.addViewController("/main").setViewName("main");
 	}
 }
---
+++ springtest/src/main/webapp/WEB-INF/view/main.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>  
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="EUC-KR">
+<title>Main</title>
+</head>
+<body>
+	<p>Welcome</p>
+	<p><a href="<c:url value="/register/step1"/>">[회원 가입하기]</a></p>
+</body>
+</html>
