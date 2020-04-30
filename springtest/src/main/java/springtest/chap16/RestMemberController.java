a4e1c3e017dbf341885164f7db23d66e895fafbb
snailvoyager <kyj88514@gmail.com>
2020-04-30 15:23:06 +0900

    @RequestBody JSON 요청처리

--- springtest/src/main/java/springtest/chap16/RestMemberController.java
+++ springtest/src/main/java/springtest/chap16/RestMemberController.java
@@ -4,15 +4,20 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import springtest.chap11.DuplicateMemberException;
 import springtest.chap11.Member;
 import springtest.chap11.MemberDao;
 import springtest.chap11.MemberRegisterService;
+import springtest.chap11.RegisterRequest;
 
 @RestController
 public class RestMemberController {
@@ -34,6 +39,17 @@ public class RestMemberController {
 			return null;
 		}
 		return member;
+	}
+	
+	@PostMapping("/api/members")
+	public void newMember(@RequestBody @Valid RegisterRequest regReq, HttpServletResponse response) throws IOException{	//검증 실패하면 400코드 응답
+		try {
+			Long newMemberId = registerService.regist(regReq);
+			response.setHeader("Location", "/api/members/" + newMemberId);
+			response.setStatus(HttpServletResponse.SC_CREATED);
+		} catch(DuplicateMemberException dupEx) {
+			response.sendError(HttpServletResponse.SC_CONFLICT);
+		}
 	}
 	
 }
